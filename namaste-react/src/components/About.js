48d2c592052353c404286fb9a22e1cd04dd12a92
Naman Nimesh <145377593+NamanNimesh@users.noreply.github.com>
2024-10-06 17:42:44 +0530

    Add files via upload

---
+++ namaste-react/src/components/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+
+    return (
+        <div>
+            <h1>
+                Hi, This is Naman,
+            </h1>
+            
+        </div>
+    )
+}
+
+export default About;
